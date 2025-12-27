ed3f4970c15521377249affbba6a368c28fa9402
Estevao <estevaopfon@gmail.com>
2025-12-27 16:33:16 -0300

    feat(noxy_examples): add multiple example files demonstrating various features and functionalities of the Noxy programming language, including data structures, algorithms, and I/O operations. This enhances the learning resources available for users and showcases the capabilities of the language.
    feat(noxy_examples): add web application example with REST API implementation for user management
    feat(stdlib): introduce HTTP client and server modules for handling HTTP requests and responses
    feat(stdlib): implement JSON parser and helpers for parsing and manipulating JSON data
    feat(stdlib): add SQLite module for database interactions
    feat(stdlib): create utility modules for string manipulation, time handling, and UUID generation
    chore(stdlib): establish a structured standard library for Noxy with various utility functions and modules

---
+++ noxy_examples/fibonacci.py
@@ -0,0 +1,21 @@
+import time
+
+start_time = time.time_ns() // 1_000_000  # now_ms()
+
+def fibonacci(n: int) -> int:
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+def imprimir_fibonacci(qtd: int) -> None:
+    i = 0
+    while qtd >= 0:
+        print(f"F({i}) = {fibonacci(i)}")
+        i = i + 1
+        qtd = qtd - 1
+
+imprimir_fibonacci(40)
+
+end_time = time.time_ns() // 1_000_000
+elapsed_time = end_time - start_time
+print(f"Tempo de execução: {elapsed_time} ms")
