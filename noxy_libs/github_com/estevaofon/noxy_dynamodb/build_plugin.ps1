3a33a91f6641e2c3080004f21a1deac7834cdc26
Estevao <estevaopfon@gmail.com>
2026-08-28 18:51:08 -0300

    chore(aws_lambda): remove AWS Lambda example files and DynamoDB plugin to clean up the repository and reduce maintenance overhead

--- noxy_libs/github_com/estevaofon/noxy_dynamodb/build_plugin.ps1
+++
@@ -1,4 +0,0 @@
-Write-Host "Building DynamoDB Plugin..."
-go mod tidy
-go build -o noxy-plugin-dynamodb.exe .
-Write-Host "Done. Created noxy-plugin-dynamodb.exe"
--- noxy_libs/github_com/estevaofon/noxy_dynamodb/build_plugin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Building DynamoDB Plugin..."
-go mod tidy
-go build -o noxy-plugin-dynamodb .
-echo "Done. Created ./noxy-plugin-dynamodb"
